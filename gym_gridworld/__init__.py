35cef999fda5561079cbfc458de65e62d3c73776
Jeasine Ma <jeasinema@gmail.com>
2019-04-12 09:46:46 +0800

    [Add] no-rgb gridworld and random gridworld

--- gym_gridworld/__init__.py
+++ gym_gridworld/__init__.py
@@ -1,6 +1,16 @@
 from gym.envs.registration import register
 
 register(
+    id='gridworldRGB-v0',
+    entry_point='gym_gridworld.envs:GridworldRGBEnv',
+)
+
+register(
     id='gridworld-v0',
     entry_point='gym_gridworld.envs:GridworldEnv',
+)
+
+register(
+    id='gridworldrandom-v0',
+    entry_point='gym_gridworld.envs:GridworldRandomEnv',
 )
--- gym_gridworld/envs/__init__.py
+++ gym_gridworld/envs/__init__.py
@@ -1,2 +1,2 @@
-from gym_gridworld.envs.gridworld_env import GridworldEnv
+from gym_gridworld.envs.gridworld_env import GridworldEnv,GridworldRandomEnv,GridworldRGBEnv
 
